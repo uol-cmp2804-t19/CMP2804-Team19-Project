68e9e9987ee7ccd563753f158be48d4ee14acccb
MasaMesgho <james_sam_whittingham@outlook.com>
2026-02-06 17:46:11 +0000

    changes to CBLogic and Tags
    added action tags for clarity/Ease of use purposes.
    CB now builds a list of actions and reads from the list to execute actions.

--- Assets/CBLogic.cs
+++ Assets/CBLogic.cs
@@ -1,17 +1,22 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CBLogic : MonoBehaviour
 {
+    public List<GameObject> ActionBlocks = new List<GameObject>();
     public GameObject Action1 = null;
     public GameObject Action2 = null;
     public GameObject Action3 = null;
 
-    // currently used for debuging ActionBlocks
-    private int actionCounter = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // AddActionBlocks(); // commented while still in testing
+
         if (Action1 == null)
         {
             Action1 = GameObject.Find("ActionBlock1");
@@ -20,6 +25,12 @@ public class CBLogic : MonoBehaviour
         else Debug.Log("'ActionBlock1' Manually Added");
 
         if (Action1 == null) Debug.LogError("'ActionBlock1' not found in Game Controller - CBLogic.");
+
+        if (Action1.tag == "Untagged")
+        {
+            Action1.tag = "ActionEmpty";
+            Debug.Log("'ActionEmpty' added to Action block 1");
+        }
 
         if (Action2 == null)
         {
@@ -30,6 +41,12 @@ public class CBLogic : MonoBehaviour
 
         if (Action2 == null) Debug.LogError("'ActionBlock2' not found in Game Controller - CBLogic.");
 
+        if (Action2.tag == "Untagged")
+        {
+            Action2.tag = "ActionEmpty";
+            Debug.Log("'ActionEmpty' added to Action block 2");
+        }
+
         if (Action3 == null)
         {
             Action3 = GameObject.Find("ActionBlock3");
@@ -38,6 +55,18 @@ public class CBLogic : MonoBehaviour
         else Debug.Log("'ActionBlock3' Manually Added");
 
         if (Action3 == null) Debug.LogError("'ActionBlock3' not found in Game Controller - CBLogic.");
+
+        if (Action3.tag == "Untagged")
+        {
+            Action3.tag = "ActionEmpty";
+            Debug.Log("'ActionEmpty' added to Action block 3");
+        }
+
+    }
+    // currently seperate from start for testing purposes, will add once guarenteed to work.
+    void AddActionBlocks()
+    {
+        
     }
 
     // Update is called once per frame
@@ -53,10 +82,47 @@ public class CBLogic : MonoBehaviour
     {
         // currently just moves the character around to debug movement
         Debug.Log("'PerformActions' called");
+        List<CBActionTypes> actions = GetActions();
 
+        if (actions.Count == 0)
+        {
+            Debug.LogError("'GetActions' in CBLogic did not return a list of actions");
+        }
+        else
+        {
+            foreach (CBActionTypes action in actions)
+            {
+                switch(action)
+                {
+                    case CBActionTypes.up:
+                        Debug.Log("CBLogics performed the 'up' action");
+                        break;
+                    case CBActionTypes.down:
+                        Debug.Log("CBLogics performed the 'down' action");
+                        break;
+                    case CBActionTypes.left:
+                        Debug.Log("CBLogics performed the 'left' action");
+                        break;
+                    case CBActionTypes.right:
+                        Debug.Log("CBLogics performed the 'right' action");
+                        break;
+                    default:
+                        Debug.LogError("'PerformActions' in CBLogic failed to identify: " + action);
+                        break;
+                }
+            }
+        }
 
         CBAction(CBActionTypes.up);
     }
+
+    List<CBActionTypes> GetActions()
+    {
+        List<CBActionTypes> actionList = new List<CBActionTypes>();
+
+        return actionList;
+    }
+
 
     void CBAction(CBActionTypes CBActionType)
     {
@@ -89,6 +155,6 @@ public class CBLogic : MonoBehaviour
         down,
         left,
         right,
-        none // ideally, this should never be used
+        none
     }
 }
