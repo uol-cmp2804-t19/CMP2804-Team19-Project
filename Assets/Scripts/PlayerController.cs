f9fc8938b07fbd9df31e5e5e3dc50a9f4b87b1bd
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2026-02-09 13:07:43 +0000

    migrate player movement behaviour to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerController : MonoBehaviour
 {
@@ -6,12 +7,7 @@ public class PlayerController : MonoBehaviour
     public FACING facing = FACING.DOWN;
     public AudioSource sound_walk = null;
     private float speed = 400.0f;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
+    public Tilemap tilemap = null;
 
     // Update is called once per frame
     // placeholder testing behaviour
@@ -38,5 +34,68 @@ public class PlayerController : MonoBehaviour
         {
             sound_walk.Play();
         }
+    }
+
+    Vector3Int GetPlayerCell()
+    {
+        if (tilemap == null)
+        {
+            //TODO add error handling
+            Debug.Log("You forgot to assign a player and/or map!");
+            return new Vector3Int(0, 0, 0);
+        }
+        else
+        {
+            Vector3Int currentCell = tilemap.WorldToCell(transform.position);
+            return currentCell;
+        }
+    }
+
+    void MovePlayerInDirection(Vector2 direction)
+    {
+        if (tilemap == null)
+        {
+            //TODO add error handling
+            Debug.Log("You forgot to assign a player and/or map!");
+            return;
+        }
+        else
+        {
+            //TODO - currently player can move diagonally, this will not be possible with coding block calls - does it need to be captured here?
+            Vector3Int targetCell = GetPlayerCell() + new Vector3Int(
+                Mathf.RoundToInt(direction.x),
+                Mathf.RoundToInt(direction.y),
+                0
+            );
+            // temporary teleport/snapping behaviour, to animate gradually eventually
+            TeleportPlayerToCell(targetCell);
+        }
+    }
+
+    void TeleportPlayerToCell(Vector3Int targetCell)
+    {
+        if (tilemap == null)
+        {
+            //TODO add error handling
+            Debug.Log("You forgot to assign a player and/or map!");
+            return;
+        }
+
+        if (!tilemap.HasTile(targetCell))
+        {
+            Debug.Log("off map");
+            return;
+        }
+
+        // snap to grid
+        transform.position =
+        tilemap.GetCellCenterWorld(tilemap.WorldToCell(transform.position)
+        );
+        //player.transform.position = tilemap.GetCellCenterWorld(targetCell);
+        // move player to center of tile
+        transform.position = tilemap.CellToWorld(targetCell) + tilemap.cellSize / 2f;
+        Debug.Log("on map");
+        PlayWalkSound();
+
     }
 }
