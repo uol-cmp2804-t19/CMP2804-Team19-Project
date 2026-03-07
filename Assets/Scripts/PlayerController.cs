e5ea000e2cf70137adb10d0f59ce3c82137fc8e0
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2026-03-07 10:30:57 +0000

    add player movement behaviour for cblogic refactor

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,38 +3,18 @@ using UnityEngine.Tilemaps;
 
 public class PlayerController : MonoBehaviour
 {
-    public enum FACING { UP, DOWN, LEFT, RIGHT };
+    public enum FACING
+    {
+        UP,
+        RIGHT,
+        DOWN,
+        LEFT
+    }
     public FACING facing = FACING.DOWN;
     public AudioSource sound_walk = null;
     private float speed = 400.0f;
     public Tilemap tilemap = null;
 
-    // Update is called once per frame
-    // placeholder testing behaviour
-    //void Update()
-    //{
-    //    float x = Input.GetAxisRaw("Horizontal");
-    //    float y = Input.GetAxisRaw("Vertical");
-
-    //    Vector3 movement = new Vector3(x, y, 0);
-    //    transform.position += movement * speed * Time.deltaTime;
-    //}
-
-    //TODO remove/tidy
-    // debug testing movement for player input
-    public void Move(Vector2 dir)
-    {
-        Vector3 movement = new Vector3(dir.x, dir.y, 0);
-        transform.position += movement * speed * Time.deltaTime;
-    }
-
-    public void PlayWalkSound()
-    {
-        if (sound_walk != null)
-        {
-            sound_walk.Play();
-        }
-    }
 
     public Vector3Int GetPlayerCell()
     {
@@ -51,6 +31,31 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// moves the player in the direction they are currently facing using previous movement code
+    /// </summary>
+    public void MovePlayerByFacing()
+    {
+        Vector2 dir = Vector2.zero;
+        switch (facing)
+        {
+            case FACING.UP:
+                dir = Vector2.up;
+                break;
+            case FACING.DOWN:
+                dir = Vector2.down;
+                break;
+            case FACING.LEFT:
+                dir = Vector2.left;
+                break;
+            case FACING.RIGHT:
+                dir = Vector2.right;
+                break;
+        }
+        MovePlayerInDirection(dir);
+    }
+
+    //TODO delay needs to be implemented
     public void MovePlayerInDirection(Vector2 direction)
     {
         if (tilemap == null)
@@ -61,17 +66,57 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
-            //TODO - currently player can move diagonally, this will not be possible with coding block calls - does it need to be captured here?
+            //player can move diagonally but by coding block calls should not
             Vector3Int targetCell = GetPlayerCell() + new Vector3Int(
                 Mathf.RoundToInt(direction.x),
                 Mathf.RoundToInt(direction.y),
                 0
             );
             // temporary teleport/snapping behaviour, to animate gradually eventually
+            //TODO add animation and delay, preventing further input
             TeleportPlayerToCell(targetCell);
         }
     }
 
+    //TODO move to playerAudioController?
+    public void PlayWalkSound()
+    {
+        if (sound_walk != null)
+        {
+            sound_walk.Play();
+        }
+    }
+
+    /// <summary>
+    /// turns the player right or left depending on the argument (right is x = +1 so true, left is x =-1 so false)
+    /// </summary>
+    /// <param name="turnRight">
+    /// true to turn right, false to turn left
+    /// </param>
+    public void TurnPlayer(bool turnRight)
+    {
+        if (turnRight)
+        {
+            switch (facing)
+            {
+                case FACING.UP:    facing = FACING.RIGHT; break;
+                case FACING.RIGHT: facing = FACING.DOWN;  break;
+                case FACING.DOWN:  facing = FACING.LEFT;  break;
+                case FACING.LEFT:  facing = FACING.UP;    break;
+            }
+        }
+        else
+        {
+            switch (facing)
+            {
+                case FACING.UP:    facing = FACING.LEFT;  break;
+                case FACING.LEFT:  facing = FACING.DOWN;  break;
+                case FACING.DOWN:  facing = FACING.RIGHT; break;
+                case FACING.RIGHT: facing = FACING.UP;    break;
+            }
+        }
+
+    //debug handling, no animation currently
     public void TeleportPlayerToCell(Vector3Int targetCell)
     {
         if (tilemap == null)
@@ -96,6 +141,6 @@ public class PlayerController : MonoBehaviour
         transform.position = tilemap.CellToWorld(targetCell) + tilemap.cellSize / 2f;
         Debug.Log("on map");
         PlayWalkSound();
-
     }
+
 }
