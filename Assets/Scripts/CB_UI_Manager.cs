3b5975b340a1456c8595e3b137f1e420edf4efe4
Daniel <newwby@gmail.com>
2026-04-04 17:11:27 +0100

    add placeholder cbUImgr

---
+++ Assets/Scripts/CB_UI_Manager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class CodeBlockUI : MonoBehaviour
+{
+    // requires references to UI objects
+    // behaviour for executing code
+    // behaviour for adding/removing from queue
+    // behaviour for auto-populating palette
+}
