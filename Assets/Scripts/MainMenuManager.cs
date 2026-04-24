3de09f696e9bd44c3040b8275086085d1889fecb
Borisz Kantor <29576302@students.lincoln.ac.uk>
2026-04-24 14:21:37 +0100

    Corrected level tracking
    Now checks Dan's config file.

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -28,7 +28,17 @@ public class MainMenuManager : MonoBehaviour
 
 	public void PlayGame()
 	{
-		int highestCompletedLevel = PlayerPrefs.GetInt("HighestCompletedLevel", 0);
+		int highestCompletedLevel = 0;
+
+		if (GameManager.Main != null && GameManager.Main.Config != null)
+		{
+			highestCompletedLevel = GameManager.Main.Config.LevelsCompleted;
+		}
+		else
+		{
+			Debug.LogWarning("GameManager or config not initialized. Defaulting to Level 1.");
+		}
+
 		int nextLevel = highestCompletedLevel + 1;
 
 		if (nextLevel > totalLevels)
