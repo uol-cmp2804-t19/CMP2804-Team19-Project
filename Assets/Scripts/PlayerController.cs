827c14995ec870107e9447710f90e5ee822927ff
Dan <newwby@gmail.com>
2026-04-25 21:05:35 +0100

    migrate debug wasd behaviour to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,6 +17,19 @@ public class PlayerController : MonoBehaviour
     public LevelMapManager level = null;
     public int zLevel = 0;
 
+    // currently used for debug movement input but could be extended for 'check next move allowed' or queueing actions
+    public float moveDelay = 0.15f;
+    private float nextMove = 0.0f;
+
+    public bool useDebugMoveWASD = true;
+    void Update()
+    {
+        if (useDebugMoveWASD == true)
+        {
+            CheckMovementInput();
+        }
+    }
+
     public Vector3Int GetPlayerCell()
     {
         if (level == null)
@@ -83,22 +96,74 @@ public class PlayerController : MonoBehaviour
         {
             switch (facing)
             {
-                case FACING.UP:    facing = FACING.RIGHT; break;
-                case FACING.RIGHT: facing = FACING.DOWN;  break;
-                case FACING.DOWN:  facing = FACING.LEFT;  break;
-                case FACING.LEFT:  facing = FACING.UP;    break;
+                case FACING.UP: facing = FACING.RIGHT; break;
+                case FACING.RIGHT: facing = FACING.DOWN; break;
+                case FACING.DOWN: facing = FACING.LEFT; break;
+                case FACING.LEFT: facing = FACING.UP; break;
             }
         }
         else
         {
             switch (facing)
             {
-                case FACING.UP:    facing = FACING.LEFT;  break;
-                case FACING.LEFT:  facing = FACING.DOWN;  break;
-                case FACING.DOWN:  facing = FACING.RIGHT; break;
-                case FACING.RIGHT: facing = FACING.UP;    break;
+                case FACING.UP: facing = FACING.LEFT; break;
+                case FACING.LEFT: facing = FACING.DOWN; break;
+                case FACING.DOWN: facing = FACING.RIGHT; break;
+                case FACING.RIGHT: facing = FACING.UP; break;
             }
         }
     }
 
+    private void CheckMovementInput()
+    {
+        if (level == null)
+        {
+            return;
+        }
+
+        // testing behaviour only
+        // TODO remove
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        Vector2 dir = new Vector2(x, y);
+
+        // is it a valid input/is a move currently allowed
+        if (dir == Vector2.zero) return;
+        if (Time.time <= nextMove) return;
+        nextMove = Time.time + moveDelay;
+
+        // erase diagonal moves and multiple direction presses, favouring right/up on axis
+        Vector2 move_direction = new Vector2(dir.x, dir.y);
+        if (x > 0.0)
+        {
+            move_direction.y = 0.0f;
+            facing = PlayerController.FACING.RIGHT;
+        }
+        else if (y > 0.0)
+        {
+            move_direction.x = 0.0f;
+            facing = PlayerController.FACING.UP;
+        }
+        else if (x < 0.0)
+        {
+            move_direction.y = 0.0f;
+            facing = PlayerController.FACING.LEFT;
+        }
+        else if (y < 0.0)
+        {
+            move_direction.x = 0.0f;
+            facing = PlayerController.FACING.DOWN;
+        }
+        else
+        {
+            // no move
+            return;
+        }
+
+        level.MovePlayerInDirection(move_direction);
+
+        // Vector3 movement = new Vector3(x, y, 0);
+        // transform.position += movement * speed * Time.deltaTime;
+
+    }
 }
