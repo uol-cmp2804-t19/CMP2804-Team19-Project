f6e23724968966e9938e8a7cfaf57551fa084ef3
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2026-05-03 22:31:05 +0100

    fix object name misspelling

--- Assets/Scripts/SettingsManager.cs
+++ Assets/Scripts/SettingsManager.cs
@@ -4,15 +4,19 @@ using UnityEngine.Audio;
 public class SettingsManager : MonoBehaviour
 {
 	public AudioMixer mainMixer;
+	private float currentVolume = 1f;
+	private bool currentIsMuted = false;
 
 	public void SetVolume(float volume)
 	{
+		currentVolume = volume;
 		float dbVolume = Mathf.Log10(Mathf.Max(0.0001f, volume)) * 20f;
 		mainMixer.SetFloat("MasterVolume", dbVolume);
 	}
 
 	public void SetMute(bool isMuted)
 	{
+		currentIsMuted = isMuted;
 		AudioListener.pause = isMuted;
 	}
 
@@ -21,8 +25,8 @@ public class SettingsManager : MonoBehaviour
 	{
 		gameObject.SetActive(false);
 		// update config on exit to prevent unnecessary file writes when adjusting settings
-		gamemanager.Main.Config.setting_volume = volume;
-		gamemanager.Main.Config.setting_isMuted = isMuted;
-		gamemanager.Main.SaveConfig();
+		GameManager.Main.Config.setting_volume = currentVolume;
+		GameManager.Main.Config.setting_isMuted = currentIsMuted;
+		GameManager.Main.SaveConfig();
 	}
 }
