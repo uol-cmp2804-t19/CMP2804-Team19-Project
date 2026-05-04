f813c70f6d8f8fcc7e596822adcb4cfc7ae864a3
Dan <newwby@gmail.com>
2026-05-04 16:08:55 +0100

    bugfix config load error

--- Assets/Scripts/Libraries/FileIO.cs
+++ Assets/Scripts/Libraries/FileIO.cs
@@ -9,6 +9,19 @@ public class FileIO
         File.WriteAllText(path, FileContent);
     }
 
+    // confirm if file exists before attempting to load or write
+    public static bool VerifyFileExists(string FilePath)
+    {
+        if (File.Exists(Path.Combine(Application.persistentDataPath, FilePath)))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
     public static string ReadFile(string FilePath)
     {
         string path = Path.Combine(Application.persistentDataPath, FilePath);
